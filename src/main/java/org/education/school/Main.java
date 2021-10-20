3faac1d056c3728e174edc1dcb7e2a8b545f9fc2
t.mukhitdinov <timurnav@gmail.com>
2021-10-21 00:26:26 +0300

    logback

--- src/main/java/org/education/school/Main.java
+++ src/main/java/org/education/school/Main.java
@@ -1,11 +1,15 @@
 package org.education.school;
 
-import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("org.education.school.config");
+        new AnnotationConfigApplicationContext("org.education.school.config");
+        logger.info("Context loaded");
     }
 }
