21b1fc4cd5f650c2a91f4eeda2739671e0647e9e
t.mukhitdinov <timurnav@gmail.com>
2021-10-21 00:27:46 +0300

    hikari data source

---
+++ src/main/java/org/education/school/config/DataSourceConfiguration.java
@@ -0,0 +1,26 @@
+package org.education.school.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource dataSource() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream stream = this.getClass().getClassLoader()
+                .getResourceAsStream("datasource.properties")) {
+            properties.load(stream);
+        }
+        HikariConfig config = new HikariConfig(properties);
+        return new HikariDataSource(config);
+    }
+}
