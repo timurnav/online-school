be379566fa62d88c4e93fa042046ac54ba620012
t.mukhitdinov <timurnav@gmail.com>
2021-11-01 01:38:27 +0300

    spring boot

---
+++ src/main/java/org/education/school/config/WebInitializer.java
@@ -0,0 +1,8 @@
+package org.education.school.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebInitializer extends SpringBootServletInitializer {
+}
