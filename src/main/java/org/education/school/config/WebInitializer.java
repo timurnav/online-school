f2d9709ea419a0582c02089f971cecfb4e781e69
t.mukhitdinov <timurnav@gmail.com>
2021-11-02 00:27:40 +0300

    get rid of spring boot

--- src/main/java/org/education/school/config/WebInitializer.java
+++ src/main/java/org/education/school/config/WebInitializer.java
@@ -1,8 +1,28 @@
 package org.education.school.config;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
-public class WebInitializer extends SpringBootServletInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+@ComponentScan
+public class WebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebInitializer.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
 }
